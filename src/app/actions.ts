43c2882595bb4c0fc2e5ecc5841fd933d769c4a8
0xZach <sylvainmasclet24@gmail.com>
2025-05-23 21:14:53 +0300

    Fix hash, add login, fix username test, put "use server" at top of file

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,16 @@
-import { db } from "../db";
-import bcrypt from "bcrypt";
+"use server";
+
+import { db } from "@/db";
+import bcrypt, { compareSync } from "bcrypt";
 import { cookies } from "next/headers";
-import { lucia } from "../lib/auth";
+import { lucia } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { generateIdFromEntropySize } from "lucia";
-import { userTable } from "../db/schema";
+import { userTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
-export async function signup(formData: FormData): Promise<ActionResult> {
-    "use server";
+
+export async function signup(formData: FormData) {
     const username = formData.get("username");
     // username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
     // keep in mind some database (e.g. mysql) are case insensitive
@@ -15,7 +18,7 @@ export async function signup(formData: FormData): Promise<ActionResult> {
         typeof username !== "string" ||
         username.length < 3 ||
         username.length > 31 ||
-        !/^[a-z0-9_-]+$/.test(username)
+        !/^[a-zA-Z0-9_-]+$/.test(username)
     ) {
         return {
             error: "Invalid username"
@@ -28,39 +31,15 @@ export async function signup(formData: FormData): Promise<ActionResult> {
         };
     }
 
-    const bcryptPassHash = () => {
-        const saltRounds = 10; // typical value is between 10 and 12
-        let res = "";
-        bcrypt.genSalt(saltRounds, (err, salt) => {
-            if (err) {
-                // Handle error
-                return;
-            }
-            const userPassword = 'user_password'; // Replace with the actual password
-            bcrypt.hash(userPassword, salt, (err, hash) => {
-                if (err) {
-                    // Handle error
-                    return;
-                }
-
-                // Hashing successful, 'hash' contains the hashed password
-                // console.log('Hashed password:', hash);
-                res = hash;
-            });
-            // Salt generation successful, proceed to hash the password
-        });
-
-        return res;
-    };
-
-    const hashed_pass = bcryptPassHash();
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
     const userId = generateIdFromEntropySize(10); // 16 characters long
 
     // TODO: check if username is already used
     await db.insert(userTable).values({
         id: userId,
         username: username,
-        password_hash: hashed_pass
+        password_hash: hash
     });
 
     const session = await lucia.createSession(userId, {});
@@ -71,7 +50,53 @@ export async function signup(formData: FormData): Promise<ActionResult> {
     return redirect("/");
 }
 
-interface ActionResult {
-    error: string;
-}
 
+export async function login(formData: FormData) {
+    const username = formData.get("username");
+    if (
+        typeof username !== "string" ||
+        username.length < 3 ||
+        username.length > 31 ||
+        !/^[a-zA-Z0-9_-]+$/.test(username)
+    ) {
+        return {
+            error: "Invalid username"
+        };
+    }
+    const password = formData.get("password");
+    if (typeof password !== "string" || password.length < 6 || password.length > 255) {
+        return {
+            error: "Invalid password"
+        };
+    }
+
+    const existingUser = db.select().from(userTable).where(eq(userTable.username, username)).limit(1).get();
+    if (!existingUser) {
+        console.log("boop");
+        // NOTE:
+        // Returning immediately allows malicious actors to figure out valid usernames from response times,
+        // allowing them to only focus on guessing passwords in brute-force attacks.
+        // As a preventive measure, you may want to hash passwords even for invalid usernames.
+        // However, valid usernames can be already be revealed with the signup page among other methods.
+        // It will also be much more resource intensive.
+        // Since protecting against this is non-trivial,
+        // it is crucial your implementation is protected against brute-force attacks with login throttling etc.
+        // If usernames are public, you may outright tell the user that the username is invalid.
+        return {
+            error: "Incorrect username or password"
+        };
+    }
+
+    const validPassword = compareSync(password, existingUser.password_hash);
+    if (!validPassword) {
+        return {
+            error: "Incorrect username or password"
+        };
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    const cookieStore = await cookies();
+    cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    return redirect("/");
+}
