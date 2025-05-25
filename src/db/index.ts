1c2c5ba094ff3647d44a94c7e2cb6d671f206219
0xZach <sylvainmasclet24@gmail.com>
2025-05-25 11:17:46 +0300

    adding explicitly schema to the definition of the db for installation purposes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,15 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import sqlite from 'better-sqlite3';
+import { sessionTable, userTable } from './schema';
 
 const sqliteDB = sqlite("sqlite.db");
-export const db = drizzle(sqliteDB);
+export const db = drizzle({
+    client: sqliteDB,
+    schema: {
+        userTable,
+        sessionTable
+    }
+});
 
 export interface DatabaseUser {
     id: string;
