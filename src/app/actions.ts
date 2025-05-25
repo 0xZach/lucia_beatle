ffd9c14ade8b64d1893f8c6014dd918090cfe07c
0xZach <sylvainmasclet24@gmail.com>
2025-05-25 10:46:48 +0300

    modify logout to work on main page instead of a specific page (leaving the logout page in just in case)

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -103,13 +103,11 @@ export async function login(currentState: { error: string }, formData: FormData)
 }
 
 
-export async function logout(currentState: { error: string }) {
+export async function logout() {
     "use server";
     const { session } = await validateRequest();
     if (!session) {
-        return {
-            error: "Unauthorized"
-        };
+        return redirect("/login");
     }
 
     await lucia.invalidateSession(session.id);
@@ -117,5 +115,5 @@ export async function logout(currentState: { error: string }) {
     const sessionCookie = lucia.createBlankSessionCookie();
     const cookieStore = await cookies();
     cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-    return redirect("/");
+    return redirect("/login");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
 import RootLayout from './layout';
 import { validateRequest } from '@/lib/validate-request';
-import Link from 'next/link';
+//import Link from 'next/link';
+import { logout } from "./actions";
+import React from "react";
+import { redirect } from 'next/navigation';
 
 async function Page() {
   const { user } = await validateRequest();
+  if (!user) {
+    return redirect("/login");
+  }
   return (
     <RootLayout>
       <h1>Home</h1>
       {user && <p>Hello {user.username}!</p>}
-      {user && <Link href="/logout">Logout</Link>}
-      {!user && <Link href="/login">Login</Link>}<br /><br />
-      {!user && <Link href="/signup">Signup</Link>}
+
+      {user &&
+        <form action={logout}>
+          <button type="submit">Sign out</button>
+        </form>
+      }
+
+
     </RootLayout>
   );
 }
+
+// {!user && <Link href="/login">Login</Link>}<br /><br />
+// {!user && <Link href="/signup">Signup</Link>}
 
 export default Page;
