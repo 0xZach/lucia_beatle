9cee72990fe0e32f7cfbf0780f5aa1e8670cff32
0xZach <sylvainmasclet24@gmail.com>
2025-05-26 09:56:48 +0300

    new 'auth' file is the one not using lucia

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,60 @@
-import { Lucia } from "lucia";
-import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
-import { DatabaseUser, db } from "@/db";
-import { sessionTable, userTable } from "../db/schema";
+import { db } from "@/db";
+import { sessionTable, userTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
+import { sha256 } from "@oslojs/crypto/sha2";
 
+import type { DatabaseUser, Session } from "@/db";
 
-const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
-
-
-export const lucia = new Lucia(adapter, {
-    sessionCookie: {
-        attributes: {
-            // set to `true` when using HTTPS
-            secure: process.env.NODE_ENV === "production"
-        }
-    },
-    getUserAttributes: (attributes) => {
-        return {
-            // attributes has the type of DatabaseUserAttributes
-            username: attributes.username
-        };
-    }
-});
-
-// IMPORTANT!
-declare module "lucia" {
-    interface Register {
-        Lucia: typeof lucia;
-        DatabaseUserAttributes: Omit<DatabaseUser, "id">;
-    }
+export function generateSessionToken(): string {
+	const bytes = new Uint8Array(20);
+	crypto.getRandomValues(bytes);
+	const token = encodeBase32LowerCaseNoPadding(bytes);
+	return token;
 }
+
+export async function createSession(token: string, userId: string): Promise<Session> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+	const session: Session = {
+		id: sessionId,
+		userId,
+		expiresAt: Date.now() + 1000 * 60 * 60 * 24 * 30
+	};
+	await db.insert(sessionTable).values(session);
+	return session;
+}
+
+export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+	const result = await db
+		.select({ user: userTable, session: sessionTable })
+		.from(sessionTable)
+		.innerJoin(userTable, eq(sessionTable.userId, userTable.id))
+		.where(eq(sessionTable.id, sessionId));
+	if (result.length < 1) {
+		return { session: null, user: null };
+	}
+	const { user, session } = result[0];
+	if (Date.now() >= session.expiresAt) {
+		await db.delete(sessionTable).where(eq(sessionTable.id, session.id));
+		return { session: null, user: null };
+	}
+	if (Date.now() >= session.expiresAt - 1000 * 60 * 60 * 24 * 15) {
+		session.expiresAt = Date.now() + 1000 * 60 * 60 * 24 * 30;
+		await db
+			.update(sessionTable)
+			.set({
+				expiresAt: session.expiresAt
+			})
+			.where(eq(sessionTable.id, session.id));
+	}
+	return { session, user };
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+	await db.delete(sessionTable).where(eq(sessionTable.id, sessionId));
+}
+
+export type SessionValidationResult =
+	| { session: Session; user: DatabaseUser }
+	| { session: null; user: null };
